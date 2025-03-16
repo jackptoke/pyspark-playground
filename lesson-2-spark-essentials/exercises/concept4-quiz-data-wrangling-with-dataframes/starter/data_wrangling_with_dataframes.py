8f8289931daa588b690580ee2c624c3e671270e7
Jack Toke <jackptoke@gmail.com>
2025-03-16 13:16:36 +1100

    Completed concept 4

--- lesson-2-spark-essentials/exercises/concept4-quiz-data-wrangling-with-dataframes/starter/data_wrangling_with_dataframes.py
+++ lesson-2-spark-essentials/exercises/concept4-quiz-data-wrangling-with-dataframes/starter/data_wrangling_with_dataframes.py
@@ -1,6 +1,9 @@
 # # Data Wrangling with DataFrames Coding Quiz
 
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import asc, desc, udf, count, col
+from pyspark.sql import Window
+from pyspark.sql.types import IntegerType
 
 # TODOS: 
 # 1) import any other libraries you might need
@@ -8,42 +11,71 @@ from pyspark.sql import SparkSession
 # 3) read in the data set located at the path "../../data/sparkify_log_small.json"
 # 4) write code to answer the quiz questions 
 
+spark = SparkSession \
+    .builder \
+    .appName("Data Frames practice") \
+    .getOrCreate()
+
+path = "../../data/sparkify_log_small.json"
+
+users_df = spark.read.json(path)
 
 # # Question 1
 # 
 # Which page did user id "" (empty string) NOT visit?
 
-# TODO: write your code to answer question 1
+# My approad
+print("My solution:")
+all_pages = users_df.select('page').drop_duplicates()
+pages_visited = users_df.filter(users_df.userId == '').select('page').drop_duplicates()
+all_pages.subtract(pages_visited).orderBy(asc("page")).show()
+
 
 
 # # Question 2 - Reflect
 # 
 # What type of user does the empty string user id most likely refer to?
 # 
-
-
-# TODO: use this space to explore the behavior of the user with an empty string
+# Users who have not signed up
 
 
 # # Question 3
 # 
 # How many female users do we have in the data set?
-
-
-# TODO: write your code to answer question 3
+number_of_female = users_df.filter(users_df.gender == 'F').select("userId").drop_duplicates().count()
+print(number_of_female)
 
 
 # # Question 4
 # 
 # How many songs were played from the most played artist?
-
-# TODO: write your code to answer question 4
+users_df.filter(users_df.page == 'NextSong') \
+    .select('Artist') \
+    .groupBy('Artist') \
+    .agg({'Artist':'count'}) \
+    .withColumnRenamed('count(Artist)', 'Playcount') \
+    .sort(desc('Playcount')) \
+    .show(1)
 
 # # Question 5 (challenge)
 # 
 # How many songs do users listen to on average between visiting our home page? Please round your answer to the closest integer.
 # 
-# 
+#
+user_window = Window \
+    .partitionBy('userID') \
+    .orderBy(desc('ts')) \
+    .rangeBetween(Window.unboundedPreceding, 0)
 
-# TODO: write your code to answer question 5
+is_home = udf(lambda page : int(page == 'Home'), IntegerType())
 
+cum_songs = users_df.filter((users_df.page == 'NextSong') | (users_df.page == 'Home')) \
+    .select("userId", "page", "ts") \
+    .withColumn("Home Visits", is_home(col("page"))) \
+    .withColumn("period", count("Home Visits").over(user_window))
+
+cum_songs.filter(cum_songs.page == "NextSong") \
+                .groupBy("userId", "period") \
+                .agg({"period":"count"}) \
+                .agg({"count(period)":"avg"}) \
+                .show()
